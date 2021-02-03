fc8f531770e92b1a7e7104b7618a0eb1918e5b47
cruzdelsur3 <59762546+cruzdelsur3@users.noreply.github.com>
2021-02-03 11:57:32 -0300

    hr

--- success.php
+++ success.php
@@ -1,5 +1,12 @@
 <?php
 
 print_r($_POST);
+echo '<hr>';
 print_r($_GET);
+echo '<hr>';
 print_r($_REQUEST);
+
+/*
+ * SUCCESS
+ * Array ( [preference_id] => 469485398-55f4dc4f-30e7-4cf4-8d2c-c7b223f5503e [external_reference] => marcos.botta@gmail.com [back_url] => [payment_id] => 13434724908 [payment_status] => approved [payment_status_detail] => accredited [merchant_order_id] => 2285985386 [processing_mode] => aggregator [merchant_account_id] => ) Array ( ) Array ( [preference_id] => 469485398-55f4dc4f-30e7-4cf4-8d2c-c7b223f5503e [external_reference] => marcos.botta@gmail.com [back_url] => [payment_id] => 13434724908 [payment_status] => approved [payment_status_detail] => accredited [merchant_order_id] => 2285985386 [processing_mode] => aggregator [merchant_account_id] => )
+ * */
